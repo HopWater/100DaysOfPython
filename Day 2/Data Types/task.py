68ce63e10aa1f66bdb06727500cd28bf5bbb1dfe
HopWater <adam.v.gonzalez@gmail.com>
2024-11-12 02:28:01 -0800

    Working on day 2.

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,21 @@
-len("Hello")
+#print(len(str(123245)))
+
+#Subscripting
+print("Hello"[3])
+
+#Strings
+print("123"+"456")
+
+#Integers
+print(123+345)
+
+#Large Integers
+#if you add _ the interpreter will remove them from the output
+print(123_123_322)
+
+#Float = Floating Point Number
+print(3.14159)
+
+#Bloolean
+print(True)
+print(False)
