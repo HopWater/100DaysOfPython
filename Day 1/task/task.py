5663c4d4e6dcaf4e39ef4d12204d2a6dc8db10de
HopWater <adam.v.gonzalez@gmail.com>
2024-11-12 02:09:40 -0800

    Finished day 1.

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,7 +1,7 @@
 #This code generates a band name based off questions asked.
 print("Band Name Generator\nAnswer these questions to create a band name!")
 
-city = input("What city did you grow up in?")
-pet = input("What is the name of your pet?")
+city = input("What city did you grow up in?\n")
+pet = input("What is the name of your pet?\n")
 
 print("Your band name is: " + city + " " + pet)
