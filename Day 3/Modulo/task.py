328e675d2cc8e89c8c72410fa8e0cace4443c3d6
HopWater <adam.v.gonzalez@gmail.com>
2024-11-13 08:16:12 -0800

    Working on Day 3.

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,11 @@
+#The answer should be 1
+#print(10%3)
+
+
+#This is a program to figure out if the number is odd or even
+number = int((input("Give me a number!")))
+
+if (number % 2) == 0:
+    print("Even")
+else:
+    print("Odd")
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -3,3 +3,23 @@ size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
 
+if size == "S" or size == "s":
+    price = 15
+    if pepperoni == "Y" or pepperoni == "y":
+        price += 2
+    if extra_cheese == "Y" or extra_cheese == "y":
+        price += 1
+elif size == "M" or size == "m":
+    price = 20
+    if pepperoni == "Y" or pepperoni == "y":
+        price += 3
+    if extra_cheese == "Y" or extra_cheese == "y":
+        price += 1
+elif size == "L" or size == "l":
+    price = 25
+    if pepperoni == "Y" or pepperoni == "y":
+        price += 3
+    if extra_cheese == "Y" or extra_cheese == "y":
+        price += 1
+
+print(f"Your final bill is: ${price}. ")
