73c3895e18a21822bca80b73c89b3f3baa5e094f
HopWater <adam.v.gonzalez@gmail.com>
2024-11-13 11:53:32 -0800

    Working on Day 3.

--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -2,6 +2,7 @@ print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M or L: ")
 pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
 extra_cheese = input("Do you want extra cheese? Y or N: ")
+price = 0
 
 if size == "S" or size == "s":
     price = 15
