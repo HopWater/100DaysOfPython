311a895ce5372898ae474d1d4ae177053dd21396
HopWater <adam.v.gonzalez@gmail.com>
2024-11-15 02:52:35 -0800

    Working on Day 4.

--- Day 4/Banker Roulette/task.py
+++ Day 4/Banker Roulette/task.py
@@ -1,1 +1,8 @@
+#This program outputs a random name based off a random generated number.
+import random
+
 friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+
+rand_number = random.randint(0,4)
+
+print(friends[rand_number])
