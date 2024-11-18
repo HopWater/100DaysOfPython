234ce49fcbec99affa720d8d5f10a687c6adc09b
HopWater <adam.v.gonzalez@gmail.com>
2024-11-18 10:05:12 -0800

    Working on Day 4.

--- Day 4/Random Module/task.py
+++ Day 4/Random Module/task.py
@@ -2,13 +2,24 @@ import random
 
 rand_num = random.randint(1, 10)
 
-print("This is a test of what number " + str(rand_num))
+#print("This is a test of what number " + str(rand_num))
 
 head_or_tail = rand_num % 2
 
-print("This is a test of what number " + str(head_or_tail))
+#print("This is a test of what number " + str(head_or_tail))
 
 if head_or_tail == 0:
-    print("Head")
+    print("Heads")
 else:
-    print("Tail")
+    print("Tails")
+
+
+
+# random_integer = random.randint(1,10)
+# print(random_integer)\
+#
+# random_number_0_to_1 = random.random() * 10
+# print(random_number_0_to_1)
+
+# random_float = random.uniform(0,10)
+# print(random_float)
